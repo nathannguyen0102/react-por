a1d53f4f075ed0c507329edac2fac529e89a51ff
NathanNguyen <khanhngoc0102@gmail.com>
2023-02-06 19:29:25 -0800

    Updates

---
+++ precache-manifest.75ba1d51b751c0b80d15cdc4d77f77e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2f88ce41d40dd18b03236d6c6ad162",
+    "url": "/react-por/index.html"
+  },
+  {
+    "revision": "1df3d101bce5b7149572",
+    "url": "/react-por/static/css/main.18eb535f.chunk.css"
+  },
+  {
+    "revision": "e0a227175f3e30fc9aed",
+    "url": "/react-por/static/js/2.2306ee73.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/react-por/static/js/2.2306ee73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a43bbbd9b51224a96bc",
+    "url": "/react-por/static/js/3.a8a4eb8b.chunk.js"
+  },
+  {
+    "revision": "1df3d101bce5b7149572",
+    "url": "/react-por/static/js/main.dd4cc329.chunk.js"
+  },
+  {
+    "revision": "f3caa4ea953fde3cf43d",
+    "url": "/react-por/static/js/runtime-main.aa18a5b0.js"
+  },
+  {
+    "revision": "b21c84b6f382661cf99794bd50c275e3",
+    "url": "/react-por/static/media/1.b21c84b6.jfif"
+  },
+  {
+    "revision": "7b0b4fda3638caa0d08a4259fd21fb1a",
+    "url": "/react-por/static/media/chores.7b0b4fda.PNG"
+  },
+  {
+    "revision": "897c4bda5178c37bd3c50b32989a69bf",
+    "url": "/react-por/static/media/connect4.897c4bda.PNG"
+  },
+  {
+    "revision": "f7ac8675d59d58a81bcd9ee46991884e",
+    "url": "/react-por/static/media/foodrecipe.f7ac8675.PNG"
+  }
+]);
